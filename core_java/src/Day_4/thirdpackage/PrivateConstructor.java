56b8c35fd92a8d119edea2d804d0e04820ebd859
bushy-ash <bushyansari30@gmail.com>
2025-09-29 22:15:21 +0530

    commit

---
+++ core_java/src/Day_4/thirdpackage/PrivateConstructor.java
@@ -0,0 +1,16 @@
+package Day_4.thirdpackage;
+
+public class PrivateConstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass m=MyClass.getObject();
+		m.setId(10);
+		
+		MyClass m1=MyClass.getObject();
+		
+		System.out.println(m);
+		System.out.println(m1);
+	}
+
+}
