3f2eac07829b1b128355de5ebaec1d66d81e06cd
bushy-ash <bushyansari30@gmail.com>
2025-11-02 21:46:58 +0530

    commit

---
+++ core_java/src/com/tnsif/daytwentyone/userdefinedannotation/AndroidSeries.java
@@ -0,0 +1,17 @@
+package com.tnsif.daytwentyone.userdefinedannotation;
+
+public class AndroidSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub@SmartTV(os = "android", height = 40, width = 50)
+		@SmartPhone()
+		class AndroidSeries {
+			String model;
+			int screenSize;
+
+			public AndroidSeries(String model, int screenSize) {
+				this.model = model;
+				this.screenSize = screenSize;
+	}
+
+}}}
